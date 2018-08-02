ba01b83012d14d0f957cf7a45cebeff37f0dd494
Chandan Kushtawar <chandan.kushtawar@icloud.com>
2018-08-02 11:41:32 +0530

    same

--- tests/medunglar.js
+++ tests/medunglar.js
@@ -2,11 +2,12 @@ module.exports = {
   before : function (browser) {
     
    // browser.resizeWindow(1000, 600);
-    browser.frameParent();
+    browser.switchWindow();
+
   },  
   'Medangular Test' : function (client) {
       client
-      
+        .pause(20000)
         .url('http://localhost:4200')
         .waitForElementVisible('body', 5000)
         .click('xpath','//button[text()="Submit"]')
@@ -15,7 +16,7 @@ module.exports = {
         .waitForElementVisible('div.row div.col.s12 div.card div.card-content.white-text div.row div.col.s5:nth-child(1) form.formheader.ng-pristine.ng-invalid.ng-untouched div.row:nth-child(6) > div.input-field.col.s12',5000)
          .waitForElementVisible('div.row div.col.s12 div.card div.card-content.white-text div.row div.col.s7:nth-child(2) table.responsive-table.highlight tbody:nth-child(2) tr:nth-child(1) td:nth-child(4) a.action-btn:nth-child(2) > i.material-icons',5000) 
          .click('.responsive-table > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(4) > a:nth-child(1) > i:nth-child(1)')
-         .pause(25000)
+       
          
          .end();
          
