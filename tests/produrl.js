cd955513ef8386d1482b7e4dfae175d77df9c7a0
Chandan Kushtawar <chandan.kushtawar@icloud.com>
2018-08-02 12:27:50 +0530

    pause

--- tests/produrl.js
+++ tests/produrl.js
@@ -16,7 +16,7 @@ module.exports = {
         .waitForElementVisible('div.row div.col.s12 div.card div.card-content.white-text div.row div.col.s5:nth-child(1) form.formheader.ng-pristine.ng-invalid.ng-untouched div.row:nth-child(6) > div.input-field.col.s12',5000)
          .waitForElementVisible('div.row div.col.s12 div.card div.card-content.white-text div.row div.col.s7:nth-child(2) table.responsive-table.highlight tbody:nth-child(2) tr:nth-child(1) td:nth-child(4) a.action-btn:nth-child(2) > i.material-icons',5000) 
          .click('.responsive-table > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(4) > a:nth-child(1) > i:nth-child(1)')
-         .pause(10000)
+       
          
          .end();
          
