344ccf538be299fca2fe28bfbc9ab5f779aa7ed5
Chandan Kushtawar <chandan.kushtawar@icloud.com>
2018-08-03 20:40:08 +0530

    produrl

--- tests/produrl.js
+++ tests/produrl.js
@@ -14,9 +14,8 @@ module.exports = {
         .click('xpath','//button[text()="Reset"]')
         //.waitForElementVisible('/html/body/app-root/app-product/div/div/div/div/div/div[1]/form/div[5]/div',5000)
         .waitForElementVisible('div.row div.col.s12 div.card div.card-content.white-text div.row div.col.s5:nth-child(1) form.formheader.ng-pristine.ng-invalid.ng-untouched div.row:nth-child(6) > div.input-field.col.s12',5000)
-         .waitForElementVisible('div.row div.col.s12 div.card div.card-content.white-text div.row div.col.s7:nth-child(2) table.responsive-table.highlight tbody:nth-child(2) tr:nth-child(1) td:nth-child(4) a.action-btn:nth-child(2) > i.material-icons',5000) 
-         .click('.responsive-table > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(4) > a:nth-child(1) > i:nth-child(1)')
-       
+         .waitForElementVisible('body.ng-scope:nth-child(2) div.row div.col.s12 div.card div.card-content.white-text div.row div.col.s7:nth-child(2) table.table tbody:nth-child(2) tr:nth-child(1) td:nth-child(4) a.action-btn:nth-child(1) > i.material-icons',5000) 
+         .click('body.ng-scope:nth-child(2) div.row div.col.s12 div.card div.card-content.white-text div.row div.col.s7:nth-child(2) table.table tbody:nth-child(2) tr:nth-child(1) td:nth-child(4) a.action-btn:nth-child(1) > i.material-icons')
          .pause(10000)
          .end();
          
