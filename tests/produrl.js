20797d1ae05591c7d9a8cda43ff41e646736cb5b
Chandan Kushtawar <chandan.kushtawar@icloud.com>
2018-08-19 19:28:41 +0530

    tests for new UI updated

--- tests/produrl.js
+++ tests/produrl.js
@@ -12,12 +12,15 @@ module.exports = {
         .waitForElementVisible('body', 5000)
         .click('xpath','//button[text()="Submit"]')
         .click('xpath','//button[text()="Reset"]')
+         .click('xpath','//button[text()="View"]')
         //.waitForElementVisible('/html/body/app-root/app-product/div/div/div/div/div/div[1]/form/div[5]/div',5000)
-        .waitForElementVisible('div.row div.col.s12 div.card div.card-content.white-text div.row div.col.s5:nth-child(1) form.formheader.ng-pristine.ng-invalid.ng-untouched div.row:nth-child(6) > div.input-field.col.s12',5000)
-         .waitForElementVisible('body.ng-scope:nth-child(2) div.row div.col.s12 div.card div.card-content.white-text div.row div.col.s7:nth-child(2) table.table tbody:nth-child(2) tr:nth-child(1) td:nth-child(4) a.action-btn:nth-child(1) > i.material-icons',5000) 
-         .click('body.ng-scope:nth-child(2) div.row div.col.s12 div.card div.card-content.white-text div.row div.col.s7:nth-child(2) table.table tbody:nth-child(2) tr:nth-child(1) td:nth-child(4) a.action-btn:nth-child(1) > i.material-icons')
-         .pause(10000)
+        .waitForElementVisible('body.ng-scope:nth-child(2) div.row div.maindiv div.col.s12 div.card div.card-content.white-text div.row div.col.s7:nth-child(3) table.table tbody:nth-child(2) tr:nth-child(1) td:nth-child(4) a.action-btn:nth-child(1) > i.material-icons',5000)
+         .waitForElementVisible('body.ng-scope:nth-child(2) div.row div.maindiv div.col.s12 div.card div.card-content.white-text div.row div.col.s7:nth-child(3) table.table tbody:nth-child(2) tr:nth-child(1) td:nth-child(4) a.action-btn:nth-child(2) > i.material-icons',5000) 
+         .click('body.ng-scope:nth-child(2) div.row div.maindiv div.col.s12 div.card div.card-content.white-text div.row div.col.s7:nth-child(3) table.table tbody:nth-child(2) tr:nth-child(1) td:nth-child(4) a.action-btn:nth-child(1) > i.material-icons')
+         .pause(30000)
+         //.click('body.ng-scope:nth-child(2) div.row div.maindiv div.col.s12 div.card div.card-content.white-text div.row div.col.s5:nth-child(2) form.formheader.ng-untouched.ng-pristine.ng-valid div.row:nth-child(6) div.input-field.col.s12 p:nth-child(1) button.btnsmall > i.glyphicon.glyphicon-info-sign',5000)
          .end();
+      //   /html/body/app-root/app-product/div/div/div/div/div/div/div[2]/form/div[5]/div/button[3]
          
     }
   };
